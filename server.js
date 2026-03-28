dc2c2583e0b1ab9c96c8367728bf63cc846f02f6
Z4eba1 <playmagazon@gmail.com>
2026-03-28 16:45:40 +0300

    Подключение БД

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2/promise');
+
 require('dotenv').config();
 const express = require('express');
 
@@ -7,3 +9,19 @@ const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'kinoweb'
+});
+
+async function initializeDatabase() {
+    await pool.execute(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            email VARCHAR(255),
+            password_hash VARCHAR(255)
+        )
+    `);
+}
